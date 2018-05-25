32e3aa0c1da5bf0f33863985c9b19ef21d3564e7
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-05-24 23:00:00 -0300

    Create API Endpoints and Message Flashing

--- project.py
+++ project.py
@@ -11,6 +11,12 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+@app.route('/restaurant/<int:restaurant_id>/menu/JSON/')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
 # Show all restaurants
 @app.route('/')
 @app.route('/restaurant/')
@@ -26,6 +32,7 @@ def newRestaurant():
         newRest = Restaurant(name=request.form['name'])
         session.add(newRest)
         session.commit()
+        flash('Restaurante criado com sucesso!')
         return redirect(url_for('showRestaurant'))
     else:
         return render_template('newRestaurant.html')
@@ -39,6 +46,7 @@ def editRestaurant(restaurant_id):
             editedRestaurant.name = request.form['name']
         session.add(editedRestaurant)
         session.commit()
+        flash('Restaurante editado com sucesso!')
         return redirect(url_for('showRestaurant'))
     else:
         return render_template('editRestaurant.html', restaurant_id = restaurant_id, restaurant = editedRestaurant)
@@ -51,6 +59,7 @@ def deleteRestaurant(restaurant_id):
     if request.method == 'POST':
         session.delete(deletedRestaurant)
         session.commit()
+        flash('Restaurante deletado com sucesso!')
         return redirect(url_for('showRestaurant'))
     else:
         return render_template('deleteRestaurant.html', restaurant_id = restaurant_id, item = deletedRestaurant)
@@ -74,6 +83,7 @@ def newMenuItem(restaurant_id):
                            restaurant_id = restaurant_id)
         session.add(newItem)
         session.commit()
+        flash('Novo item criado com sucesso!')
         return redirect(url_for('showMenu', restaurant_id = restaurant_id))
     else:
         return render_template('newMenu.html', restaurant_id = restaurant_id )
@@ -93,6 +103,7 @@ def editMenuItem(restaurant_id, menu_id):
             editedItem.course = request.form['course']
         session.add(editedItem)
         session.commit()
+        flash('Novo item editado com sucesso!')
         return redirect(url_for('showMenu', restaurant_id = restaurant_id))
     else:
         return render_template('editMenu.html', restaurant_id = restaurant_id, menu_id = menu_id, item = editedItem)
@@ -104,6 +115,7 @@ def deleteMenuItem(restaurant_id, menu_id):
     if request.method == 'POST':
         session.delete(deletedItem)
         session.commit()
+        flash('Item deletado com sucesso!')
         return redirect(url_for('showMenu', restaurant_id = restaurant_id))
     else:
         return render_template('deleteMenu.html', restaurant_id = restaurant_id, menu_id = menu_id, item = deletedItem)
@@ -111,5 +123,6 @@ def deleteMenuItem(restaurant_id, menu_id):
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
